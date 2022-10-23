ffb0c66113067916edba38c1e0ffeba4a41b5842
Nana Kai-Hui You <nana39193@gmail.com>
2022-10-24 01:06:51 +0800

    Update main.py

--- method/3_image_inpainting/implementation/main.py
+++ method/3_image_inpainting/implementation/main.py
@@ -1,37 +1,39 @@
-import tensorflow as tf
+import tensorflow._api.v2.compat.v1 as tf
+tf.disable_v2_behavior()
+
 from models.DNN import *
 
 # Datasets
 ######### Write directories of datasets
-tf.app.flags.DEFINE_string('train_images_dir', '', 'Training images data directory.')
-tf.app.flags.DEFINE_string('val_images_dir', '', 'Validation images data directory.')
-tf.app.flags.DEFINE_string('test_images_dir', '', 'Testing images data directory.')
+tf.compat.v1.app.flags.DEFINE_string('train_images_dir', '', 'Training images data directory.')
+tf.compat.v1.app.flags.DEFINE_string('val_images_dir', '', 'Validation images data directory.')
+tf.compat.v1.app.flags.DEFINE_string('test_images_dir', '', 'Testing images data directory.')
 # tf.app.flags.DEFINE_string('vgg_path', '', 'vgg directory.')
  # 
-tf.app.flags.DEFINE_boolean('train', True, 'whether to train the network')
-tf.app.flags.DEFINE_integer('num_epochs', 7, 'epochs to train')
-tf.app.flags.DEFINE_integer('train_batch_size', 1, 'number of elements in a training batch')
-tf.app.flags.DEFINE_integer('val_batch_size', 1, 'number of elements in a validation batch')
-tf.app.flags.DEFINE_integer('test_batch_size', 1, 'number of elements in a testing batch')
+tf.compat.v1.app.flags.DEFINE_boolean('train', True, 'whether to train the network')
+tf.compat.v1.app.flags.DEFINE_integer('num_epochs', 7, 'epochs to train')
+tf.compat.v1.app.flags.DEFINE_integer('train_batch_size', 1, 'number of elements in a training batch')
+tf.compat.v1.app.flags.DEFINE_integer('val_batch_size', 1, 'number of elements in a validation batch')
+tf.compat.v1.app.flags.DEFINE_integer('test_batch_size', 1, 'number of elements in a testing batch')
 
-tf.app.flags.DEFINE_integer('height_of_image', 256, 'Height of the images.')
-tf.app.flags.DEFINE_integer('width_of_image', 256, 'Width of the images.')
-tf.app.flags.DEFINE_float('num_channels', 3, 'Number of the channels of the images.')
+tf.compat.v1.app.flags.DEFINE_integer('height_of_image', 256, 'Height of the images.')
+tf.compat.v1.app.flags.DEFINE_integer('width_of_image', 256, 'Width of the images.')
+tf.compat.v1.app.flags.DEFINE_float('num_channels', 3, 'Number of the channels of the images.')
 
-tf.app.flags.DEFINE_float('learning_rate', 0.00005, 'Learning rate of the optimizer')
+tf.compat.v1.app.flags.DEFINE_float('learning_rate', 0.00005, 'Learning rate of the optimizer')
 
-tf.app.flags.DEFINE_integer('display_step', 1, 'Number of steps we cycle through before displaying detailed progress.')
-tf.app.flags.DEFINE_integer('validation_step', 1, 'Number of steps we cycle through before validating the model.')
+tf.compat.v1.app.flags.DEFINE_integer('display_step', 1, 'Number of steps we cycle through before displaying detailed progress.')
+tf.compat.v1.app.flags.DEFINE_integer('validation_step', 1, 'Number of steps we cycle through before validating the model.')
 ###### write somethong like "/Users/mariam/Desktop/ASDS/tez/autoencoder/results"
-tf.app.flags.DEFINE_string('base_dir', '', 'Directory in which results will be stored.')
-tf.app.flags.DEFINE_integer('checkpoint_step', 1, 'Number of steps we cycle through before saving checkpoint.')
-tf.app.flags.DEFINE_integer('max_to_keep',2, 'Number of checkpoint files to keep.')
+tf.compat.v1.app.flags.DEFINE_string('base_dir', '', 'Directory in which results will be stored.')
+tf.compat.v1.app.flags.DEFINE_integer('checkpoint_step', 1, 'Number of steps we cycle through before saving checkpoint.')
+tf.compat.v1.app.flags.DEFINE_integer('max_to_keep',2, 'Number of checkpoint files to keep.')
 
-tf.app.flags.DEFINE_integer('summary_step', 2, 'Number of steps we cycle through before saving summary.')
+tf.compat.v1.app.flags.DEFINE_integer('summary_step', 2, 'Number of steps we cycle through before saving summary.')
 
-tf.app.flags.DEFINE_string('model_name', 'inpainting', 'name of model')
+tf.compat.v1.app.flags.DEFINE_string('model_name', 'inpainting', 'name of model')
 
-FLAGS = tf.app.flags.FLAGS
+FLAGS = tf.compat.v1.app.flags.FLAGS
 
 
 def main(argv=None):
@@ -66,4 +68,4 @@ def main(argv=None):
 
 
 if __name__ == "__main__":
-    tf.app.run()
+    tf.compat.v1.app.run()
